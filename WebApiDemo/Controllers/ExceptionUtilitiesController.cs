091e9f8773f36cdb61731de805436cb44770eab7
Anton Georgesco <antongeorgescu@gmail.com>
2022-11-04 16:33:38 -0400

    updates to Logger and WebApi controllers

---
+++ WebApiDemo/Controllers/ExceptionUtilitiesController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text.RegularExpressions;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography.Xml;
+using Microsoft.Extensions.Configuration;
+using WebApiDemo.Utilities;
+using System.Drawing;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExceptionUtilitiesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ExceptionUtilitiesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($"MLUtilities API started at {DateTime.Now}");
+        }
+
+        [HttpGet("generatetext/{count}")]
+        public ActionResult<string> GenerateText(int count)
+        {
+            var lstWords = ServiceUtilities.GenerateSyntheticMessage(count);
+            return Ok(string.Join(' ',lstWords));
+        }
+
+        [HttpGet("exceptionlist/{framework}")]
+        public ActionResult<IEnumerable<string>> GetExceptionList(string framework)
+        {
+            var exceptions = _configuration.GetSection($"Exceptions:{framework}").GetChildren();
+            return Ok(exceptions);
+        }
+
+        [HttpPost("throwexception/instance")]
+        public ActionResult<string> ThrowErrorInstance([FromForm] ThrowErrorRequest errorRequest)
+        {
+            if (string.IsNullOrEmpty(errorRequest.Code))
+                return BadRequest();
+
+            var exceptionAttributes = _configuration.GetSection($"Exceptions:{errorRequest.Framework}")
+                                        .GetChildren()
+                                        .ToList()
+                                        .First(x => x.Key == errorRequest.Code).Value.Split('|');
+            
+            errorRequest.Class = exceptionAttributes[0];
+            errorRequest.Description = exceptionAttributes[1];
+
+            errorRequest.Generate();
+
+            return Ok(errorRequest);
+        }
+
+        [HttpPost("throwexception/serialized")]
+        public ActionResult<string> ThrowErrorSerialized([FromForm] ThrowErrorRequest errorRequest)
+        {
+            if (string.IsNullOrEmpty(errorRequest.Code))
+                return BadRequest();
+
+            var exceptionAttributes = _configuration.GetSection($"Exceptions:{errorRequest.Framework}")
+                                        .GetChildren()
+                                        .ToList()
+                                        .First(x => x.Key == errorRequest.Code).Value.Split('|');
+
+            errorRequest.Class = exceptionAttributes[0];
+            errorRequest.Description = exceptionAttributes[1];
+
+            errorRequest.Generate();
+
+            return Ok(errorRequest.Serialized);
+        }
+    }
+}
---
+++ WebApiDemo/ThrowErrorRequest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Drawing;
+using System.Security;
+using WebApiDemo.Utilities;
+
+namespace WebApiDemo
+{
+    public class ThrowErrorRequest
+    {
+        private string _code;
+        private string _framework; 
+        private string _class;
+        private string _message;
+        private string _description;
+        private dynamic _serialized = null;
+        public string Code 
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+        public string Framework
+        {
+            get { return _framework; }
+            set { _framework = value; }
+        }
+        public string Serialized
+        {
+            get { return _serialized; }
+            set { _serialized = value; }
+        }
+        public string Class
+        {
+            get { return _class; }
+            set { _class = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+        public void Generate(int wordcount = 0)
+        {
+            if (wordcount == 0)
+            {
+                // generate a random # words between 5 and 15
+                Random res = new Random();
+                wordcount = res.Next(5, 15);
+            }
+            _message = string.Join(' ', ServiceUtilities.GenerateSyntheticMessage(wordcount));
+            _serialized = $"CLASS:{_class}|DESCRIPTION:{_description}|MESSAGE:{_message}";
+            return;
+        }
+    }
+}
