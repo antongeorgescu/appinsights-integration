14ae71670f3f966375efcbd68a183ad1181ffca6
Anton Georgescu <anton.georgescu@dh.com>
2022-11-27 16:37:02 -0500

    added Sql Server database project to the solution

---
+++ SyntheticDataDb/dbo/Tables/ContentType.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ContentType] (
+    [ContentTypeID] SMALLINT      NOT NULL,
+    [Code]          VARCHAR (10)  NOT NULL,
+    [Name]          VARCHAR (50)  NOT NULL,
+    [Description]   VARCHAR (300) NULL,
+    CONSTRAINT [PK_ContentType] PRIMARY KEY CLUSTERED ([ContentTypeID] ASC)
+);
+
